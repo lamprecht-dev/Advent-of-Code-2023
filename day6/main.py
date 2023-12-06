72f275627773bbbcf06f149318588d8309a8f835
David Lamprecht <44062000+lamprecht-dev@users.noreply.github.com>
2023-12-06 13:51:02 -0600

    d6 implemented binary search version
    not really needed for the small values I received but this would reduce O(n) to O(log(n))

--- day6/main.py
+++ day6/main.py
@@ -2,8 +2,7 @@ from utils import *
 
 
 def solve(d):
-    ways_to_beat = 1
-    ways_to_beat2 = 0
+    wtb = 1
 
     btime = ""
     bthresh = ""
@@ -12,23 +11,43 @@ def solve(d):
     for i in range(1, len(ww[0])):
         btime = btime + ww[0][i]
         bthresh = bthresh + ww[1][i]
-        wtb = 0
-        time, threshold = int(ww[0][i]), int(ww[1][i])
-        for t in range(time):
-            distance = (time - t) * t
-            if distance > threshold:
-                wtb += 1
-        ways_to_beat *= wtb
+        wtb *= ways_to_beat_simple(int(ww[0][i]), int(ww[1][i]))
 
-    btime = int(btime)
-    bthresh = int(bthresh)
+    return wtb, ways_to_beat_binary_search(int(btime), int(bthresh))
 
-    for t in range(btime):
-        distance = (btime - t) * t
-        if distance > bthresh:
-            ways_to_beat2 += 1
 
-    return ways_to_beat, ways_to_beat2
+def ways_to_beat_binary_search(time, threshold):
+    # Since the values will have symitry it is easier to just search on one side at a time
+    # We want to move two pointers towards one another until we find the spot where they are just beating the thresh
+
+    bottom = 0  # Not beating threshold
+    top = time // 2  # Beating threshold
+
+    # check if top is also inside threshold
+    if top * (time - top) <= threshold:
+        return 0
+
+    while top - bottom > 1:
+        half = (top - bottom) // 2 + bottom
+        dist = half * (time - half)
+
+        if dist > threshold:
+            top = half
+        else:
+            bottom = half
+
+    # top now really is the bottom part of the range. Top is mirrored on the other side
+    return time - (2 * top) + 1
+
+
+def ways_to_beat_simple(time, threshold):
+    wtb = 0
+    for t in range(time):
+        distance = (time - t) * t
+        if distance > threshold:
+            wtb += 1
+
+    return wtb
 
 
 def main():
