19756050722427c4e107524eb8c0ccf2964347fd
David Lamprecht <44062000+lamprecht-dev@users.noreply.github.com>
2023-12-08 12:18:09 -0600

    d8 including lcm

--- day8/main.py
+++ day8/main.py
@@ -1,76 +1,43 @@
-import collections
-import datetime
-import itertools
-import functools
-import math
-from operator import itemgetter as ig
 import pprint as pp
-import re
-# import bisect
-# import heapq
-# import sys
-# sys.setrecursionlimit(1000000)
 
 from utils import *
 
 
-def s(d, part):
+def s(d):
     ll = lines(d)
     ins = [0 if c == "L" else 1 for c in ll[0]]
     ins2 = []
 
     directions = {}
 
-    gcur = []
-
     for line in ll[2:]:
         direction = line.split(" = ")
         directions[direction[0]] = direction[1].strip("()").split(", ")
-        if direction[0][2] == "A":
-            gcur.append(direction[0])
 
     steps = 0
-    if part == 1:
-        cur = "AAA"
-        tar = "ZZZ"
-        while cur != tar:
-            i = ins.pop(0)
-            cur = directions[cur][i]
+    cur = "AAA"
+    tar = "ZZZ"
+    while cur != tar:
+        i = ins.pop(0)
+        cur = directions[cur][i]
 
-            steps += 1
-            ins2.append(i)
-            if len(ins) == 0:
-                ins = ins2
-    else:
-        while sum([0 if c[2] == "Z" else 1 for c in gcur]) != 0:
-            i = ins.pop(0)
-            ngcur = []
-            for c in gcur:
-                ngcur.append(directions[c][i])
-            gcur = ngcur
-
-            steps += 1
-            ins2.append(i)
-            if len(ins) == 0:
-                ins = ins2
+        steps += 1
+        ins2.append(i)
+        if len(ins) == 0:
+            ins = ins2
 
     return steps
 
 
-def solve2(d):
+def s2(d):
     ll = lines(d)
     ins = [0 if c == "L" else 1 for c in ll[0]]
-    ins2 = []
 
     directions = {}
-
-    gcur = []
 
     for line in ll[2:]:
         direction = line.split(" = ")
         directions[direction[0]] = direction[1].strip("()").split(", ")
-        if direction[0][2] == "A":
-            gcur.append(direction[0])
 
     zmap = {}
     gcur = {}
@@ -78,61 +45,62 @@ def solve2(d):
     for d in directions:
         phasemap = {}
         if d[2] == "Z" or d[2] == "A":
-            for i in range(len(ins)):
-                if i > 0 and d[2] == "A":
+            seen = set()
+            cur = d
+            s = 0
+            tempins = ins[:]
+            tempins2 = []
+            while True:
+                t = tempins.pop(0)
+                cur = directions[cur][t]
+
+                if (s % len(ins), cur) in seen:
+                    s = -1
                     break
-                tempins = ins[i:] + ins[:i]
-                tempins2 = []
-                seen = set()
-                cur = d
-                s = 0
-                while True:
-                    t = tempins.pop(0)
-                    cur = directions[cur][t]
+                seen.add((s % len(ins), cur))
 
-                    if (s % len(ins), cur) in seen:
-                        s = -1
-                        break
-                    seen.add((s % len(ins), cur))
+                s += 1
+                tempins2.append(t)
+                if len(tempins) == 0:
+                    tempins = tempins2
 
-                    s += 1
-                    tempins2.append(t)
-                    if len(tempins) == 0:
-                        tempins = tempins2
-
-                    if cur[2] == "Z":
-                        break
-
-                if s != -1:
-                    phasemap[i] = (s, cur)
+                if cur[2] == "Z":
+                    break
 
             if d[2] == "Z":
-                zmap[d] = phasemap
+                zmap[d] = (s, cur, s % len(ins))
 
             if d[2] == "A":
-                gcur[d] = phasemap[0]
+                gcur[d] = (s, cur, s % len(ins))
 
-    pp.pprint(gcur)
-    pp.pprint(zmap)
+    # pp.pprint(gcur)
+    # pp.pprint(zmap['11Z'][0])
 
-    overal_step = 0
+    # return general_solution_attempt(cur, zmap, len(inp))
 
-    while not in_sync(gcur):
+    # This solution only works for the types of inputs we are getting.
+    # We are trying to get the lowest common multiple
+
+    return math.lcm(*[zmap[x][0] for x in zmap])
+
+
+def general_solution_attempt(cur, zmap, phase_length):
+    steps = 0
+
+    while not in_sync(cur):
         smallest = None
-        for i in gcur:
-            c = gcur[i]
-            if smallest is None or gcur[smallest][0] > c[0]:
+        for i in cur:
+            c = cur[i]
+            if smallest is None or cur[smallest][0] > c[0]:
                 smallest = i
-        sv = gcur[smallest]
+        sv = cur[smallest]
 
-        new_value = zmap[sv[1]][sv[0] % len(ins)]
-        gcur[smallest] = (new_value[0] + sv[0], new_value[1])
-        overal_step = new_value[0] + sv[0]
+        new_value = zmap[sv[1]][sv[0] % phase_length]
+        cur[smallest] = (new_value[0] + sv[0], new_value[1])
+        steps = new_value[0] + sv[0]
 
+    return steps
 
-    #  16531 15989 14363  19241 19783 21409
-
-    return overal_step
 
 def in_sync(states):
     first = None
@@ -147,7 +115,7 @@ def in_sync(states):
 def main():
     if test():
         file = inp(os.path.join(os.path.dirname(__file__), 'input.txt'))
-        solutions = (s(file, 1), solve2(file))
+        solutions = (s(file), s2(file))
         print("\n\n" + BColors.HEADER + "Solutions" + BColors.ENDC)
         for sol in solutions:
             print(sol)
@@ -174,7 +142,7 @@ ZZZ = (ZZZ, ZZZ)"""
 XXX = (XXX, XXX)"""
     a1 = 6
     a2 = 6
-    return validate_solution((s(example, 1), solve2(example2)), (a1, a2))
+    return validate_solution((s(example), s2(example2)), (a1, a2))
 
 
 main()
