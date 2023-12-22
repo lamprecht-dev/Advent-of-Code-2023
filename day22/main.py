eda7a6d0e0337e7cd584efa6a50aac8d13ed322e
David Lamprecht <44062000+lamprecht-dev@users.noreply.github.com>
2023-12-22 12:06:19 -0600

    d22 remove old code

--- day22/main.py
+++ day22/main.py
@@ -1,88 +1,9 @@
 import copy
 import sys
 import collections
-import datetime
-import itertools
-import functools
-import math
-from operator import itemgetter as ig
-import pprint as pp
-import re
-# import bisect
-# import heapq
-# sys.setrecursionlimit(1000000)
 
 sys.path.append('../')
 from utils import *
-
-class Brick:
-    def __init__(self, b1, b2):
-        b1 = [int(b) for b in b1.split(",")]
-        b2 = [int(b) for b in b2.split(",")]
-
-        self.pfrom = (min(b1[0], b2[0]), min(b1[1], b2[1]), min(b1[2], b2[2]))
-        self.pto = (max(b1[0], b2[0]), max(b1[1], b2[1]), max(b1[2], b2[2]))
-
-        self.dim = (abs(b1[0] - b2[0]) + 1, abs(b1[1] - b2[1]) + 1, abs(b1[2] - b2[2]) + 1)
-
-    def move_down(self, height_map):
-        diff = self.get_diff(height_map)
-        # self.print()
-        self.pfrom = (self.pfrom[0], self.pfrom[1], self.pfrom[2] - diff)
-        self.pto = (self.pto[0], self.pto[1], self.pto[2] - diff)
-        # self.print()
-        # print(diff)
-
-        for c in range(self.dim[0]):
-            for r in range(self.dim[1]):
-                c2 = c + self.pfrom[0]
-                r2 = r + self.pfrom[1]
-                height_map[(c2, r2)] = self.pto[2]
-
-        return height_map, self.pfrom[2], self
-
-    def can_move(self, height_map):
-        return self.get_diff(height_map) > 0
-
-    def get_diff(self, height_map):
-        max_value = 0
-        for c in range(self.dim[0]):
-            for r in range(self.dim[1]):
-                c2 = c + self.pfrom[0]
-                r2 = r + self.pfrom[1]
-                max_value = max(max_value, height_map[(c2, r2)])
-
-        return self.pfrom[2] - max_value - 1
-
-    def get_new_submap(self, height_map):
-        diff = self.get_diff(height_map)
-        submap = collections.defaultdict(int)
-        for c in range(self.dim[0]):
-            for r in range(self.dim[1]):
-                c2 = c + self.pfrom[0]
-                r2 = r + self.pfrom[1]
-                submap[(c2, r2)] = self.pto[2] - diff
-        return submap, self.pto[2] - diff
-
-    def print(self):
-        print(self.pfrom, self.pto, self.dim, math.prod(self.dim))
-
-    def overlap(self, other):
-        for c in range(self.dim[0]):
-            for r in range(self.dim[1]):
-                c2 = c + self.pfrom[0]
-                r2 = r + self.pfrom[1]
-                # print(c2, range(other.pfrom[0], other.pto[0] + 1), c2 in range(other.pfrom[0], other.pto[0] + 1))
-                # print(r2, range(other.pfrom[1], other.pto[1] + 1), r2 in range(other.pfrom[1], other.pto[1] + 1))
-                # print()
-                if (c2 in range(other.pfrom[0], other.pto[0] + 1) and r2 in range(other.pfrom[1], other.pto[1] + 1)
-                        and self.pto[2] + 1 == other.pfrom[2]):
-                    # print(range(self.pfrom[0], self.pto[0]), range(self.pfrom[1], self.pto[1]), self.pto[2], range(other.pfrom[0], other.pto[0]), range(other.pfrom[1], other.pto[1]), other.pfrom[2])
-                    # self.print()
-                    # other.print()
-                    # print()
-                    return True
-        return False
 
 
 def s(d):
